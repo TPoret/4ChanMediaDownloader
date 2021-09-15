6bf6cd05f4428359485eff56652d135b9e5e7625
BarbDev <tanguy.poret@hotmail.fr>
2021-09-16 00:19:36 +0200

    limit the number of concurrent download

--- background_script.js
+++ background_script.js
@@ -1,3 +1,6 @@
+// TODO : fix bug when multiple tab are opened (one cannot trigger download)
+// TODO : add a cancel all download when bulk is launched
+
 function sanitizeInput(input) {
     return input.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/"/g, "&quot;");
 }
@@ -14,39 +17,80 @@ function is4chanCdnUrl(url) {
 }
 
 const downloads = {};
+const downloadQueue = [];
+const MAX_CONCURRENT_DOWNLOAD = 3;
+let currentConcurrentDownload = 0;
+
+function addToDownloadQueue({url, filename, channel}) {
+    downloadQueue.push({url, filename, channel});
+}
+
+function getNextDownloadItem() {
+    return downloadQueue.shift();
+}
+
+function triggerNewDownload() {
+    if (currentConcurrentDownload >= MAX_CONCURRENT_DOWNLOAD) return;
+
+    const nextDownload = getNextDownloadItem();
+
+    if (nextDownload === undefined) return;
+
+    // Optimist incrementation is essential as it keeps calling this part before triggering the 'then()' code in another JS eventloop
+    currentConcurrentDownload++;
+
+    browser
+    .downloads
+    .download({
+        url: nextDownload.url,
+        filename: nextDownload.filename,
+        conflictAction: "uniquify",
+    })
+    .then(
+        (id) => {
+            downloads[id] = { channel: nextDownload.channel };
+            nextDownload.channel.postMessage({ event: "downloadStarted", url: nextDownload.url, id });
+        },
+        (id) => {
+            nextDownload.channel.postMessage({ event: "message", message: `Failed downloading ${id}` })
+            addToDownloadQueue(nextDownload);
+            triggerNewDownload();
+            // Should we decrement or does it also trigger a download state changed ?
+        }
+    );
+}
 
 function connected(channel) {
+    channel.postMessage({ event: "message", message: `connected` })
     channel.onMessage.addListener(function (message) {
         message = sanitizeMessage(message);
 
         if (!is4chanCdnUrl(message.url)) return;
 
-        browser.downloads
-            .download({
-                url: message.url,
-                filename: message.filename,
-                conflictAction: "uniquify",
-            })
-            .then(
-                (id) => {
-                    downloads[id] = { channel };
-                    channel.postMessage({ event: "downloadStarted", url: message.url, id });
-                },
-                (id) => channel.postMessage({ event: "message", message: `Failed downloading ${id}` })
-            );
+        addToDownloadQueue({ ...message, channel });
+        triggerNewDownload();
     });
 }
 
 function onDownloadChange(downloadItem) {
+    triggerNewDownload();
+
     if (downloadItem.state === undefined || downloadItem.state === null) return;
 
+    const state = typeof downloadItem.state === "string" ? downloadItem.state : downloadItem.state.current;
     downloads[downloadItem.id].channel.postMessage({
         event: "downloadChanged",
         id: downloadItem.id,
-        state: typeof downloadItem.state === "string" ? downloadItem.state : downloadItem.state.current,
+        state,
     });
 
-    if (downloadItem.state === "interrupted" || downloadItem.state === "complete") delete downloads[downloadItem.id];
+    if (state === "interrupted" || state === "complete") {
+        delete downloads[downloadItem.id];
+        currentConcurrentDownload--;
+        if (currentConcurrentDownload < 0) currentConcurrentDownload = 0;
+    }
+    
+    triggerNewDownload();
 }
 
 browser.runtime.onConnect.addListener(connected);
