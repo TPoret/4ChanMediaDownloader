b557c8019a878ed0dfc1335f62cc7d261a5babb0
tporet <tanguy.poret@hotmail.fr>
2026-06-22 20:17:20 +0200

    feat: add yarn release command

---
+++ scripts/release.js
@@ -0,0 +1,34 @@
+const readline = require('readline');
+const fs = require('fs');
+const { execSync } = require('child_process');
+
+const SEMVER = /^\d+\.\d+\.\d+$/;
+
+function updateJsonVersion(filePath, version) {
+  const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+  data.version = version;
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2) + '\n');
+}
+
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+
+rl.question('New version (X.Y.Z): ', (version) => {
+  rl.close();
+
+  if (!SEMVER.test(version)) {
+    console.error(`Error: "${version}" is not a valid version. Use X.Y.Z format.`);
+    process.exit(1);
+  }
+
+  updateJsonVersion('manifest.json', version);
+  updateJsonVersion('package.json', version);
+  console.log(`Updated manifest.json and package.json to ${version}`);
+
+  execSync('git add manifest.json package.json', { stdio: 'inherit' });
+  execSync(`git commit -m "chore: bump version to ${version}"`, { stdio: 'inherit' });
+  execSync(`git tag v${version}`, { stdio: 'inherit' });
+  execSync('git push', { stdio: 'inherit' });
+  execSync('git push --tags', { stdio: 'inherit' });
+
+  console.log(`Released v${version}`);
+});
